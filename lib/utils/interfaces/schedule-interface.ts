ce8bdc382071e9dfd09441edb68063d4f5b48b1c
Juliocesarramirez <julio_marshal@hotmail.com>
2019-04-02 21:12:18 -0600

    feat(createSchedule): added interface and fixed methods

---
+++ lib/utils/interfaces/schedule-interface.ts
@@ -0,0 +1,10 @@
+export interface ScheduleInterface {
+  teams: number;
+  groups: number;
+  clasification: number;
+  numberGroup: number;
+  numberTeamGroup: number;
+  excededGroups: number;
+  completeGroups: number;
+  correctTeams: number;
+}
