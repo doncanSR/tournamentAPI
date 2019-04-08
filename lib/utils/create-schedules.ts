55bad2c35bdd63a09fb304db982d1c3e29172a0c
Juliocesarramirez <julio_marshal@hotmail.com>
2019-04-07 19:49:23 -0500

    fix(create-schedules): fixed total clasifications

--- lib/utils/create-schedules.ts
+++ lib/utils/create-schedules.ts
@@ -3,11 +3,12 @@ export class createSchedule implements ScheduleInterface {
   teams: number;
   groups: number;
   clasification: number;
-  numberGroup: number;
-  numberTeamGroup: number;
+  teamsPerGroup: number;
   excededGroups: number;
   completeGroups: number;
   correctTeams: number;
+  totalClasifications:number;
+  teamsPerGruopExc:number;
 
   constructor(teams: number) {
     this.teams = teams;
@@ -29,6 +30,7 @@ export class createSchedule implements ScheduleInterface {
     if (this.teams >= 25) {
       this.createClasification();
       this.numberGroups();
+      this.clasifications();
       this.numberTeamPerGroup();
       this.nGroupsExceded();
       this.groupInitialValue();
@@ -49,34 +51,42 @@ export class createSchedule implements ScheduleInterface {
   }
 
   private numberGroups(): void {
-    this.numberGroup = (this.teams / this.clasification);
-    this.numberGroup = parseInt(this.numberGroup.toFixed(1));
-    console.log('This is the number of group ----> ', this.numberGroup);
+    this.groups = (this.teams / this.clasification);
+    this.groups = parseInt(this.groups.toFixed(1));
+    console.log('This is the number of group ----> ', this.groups);
   }
 
   private numberTeamPerGroup(): void {
-    this.numberTeamGroup = (this.teams / this.numberGroup);
-    this.excededGroups = (this.numberTeamGroup - Math.floor(this.numberTeamGroup));
-    this.numberTeamGroup = parseInt(this.numberTeamGroup.toFixed(1));
-    console.log('This is the teams per group ----> ', this.numberTeamGroup);
+    this.teamsPerGroup = (this.teams / this.groups);
+    this.excededGroups = (this.teamsPerGroup - Math.floor(this.teamsPerGroup));
+    this.teamsPerGroup = parseInt(this.teamsPerGroup.toFixed(1));
+    console.log('This is the teams per group ----> ', this.teamsPerGroup);
     console.log('This is the rest ----> ', this.excededGroups);
   }
 
   private nGroupsExceded(): void {
-    this.excededGroups = parseInt((this.excededGroups * this.numberGroup).toFixed());
+    this.excededGroups = parseInt((this.excededGroups * this.groups).toFixed());
     console.log('This is the number of excedded grups: ', this.excededGroups);
   }
 
   private groupInitialValue(): void {
-    this.completeGroups = (this.numberGroup - this.excededGroups);
+    this.completeGroups = (this.groups - this.excededGroups);
+    this.teamsPerGruopExc = (this.teamsPerGroup + 1);
     console.log('This is the number of groups with the initial team values: ', this.completeGroups);
     console.log('This is the number of groups with an extra team: ', this.excededGroups);
+    console.log('This is the number of extra teams ', this.teamsPerGruopExc);
+  }
+
+  private clasifications(): void {
+    this.totalClasifications = (this.groups / this.clasification);
+    this.totalClasifications = parseInt(this.totalClasifications.toFixed())
+    console.log('This is the clasifications total: ', this.totalClasifications);
   }
 
   public verifyTeams(): boolean {
     let isCorrect: boolean;
     this.buildSchedule();
-    this.correctTeams = (this.excededGroups * (this.numberTeamGroup + 1)) + (this.numberTeamGroup * this.completeGroups);
+    this.correctTeams = (this.excededGroups * (this.teamsPerGroup + 1)) + (this.teamsPerGroup * this.completeGroups);
     isCorrect = (this.teams === this.correctTeams) ? true : false;
     return isCorrect;
   }
